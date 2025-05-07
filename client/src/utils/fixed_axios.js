6acef356fdd40dca6c8bb8040c88a3aab52a5699
ByatesEduard <edikbatec1@gmail.com>
2025-05-07 11:40:52 +0300

    07.05

--- client/src/utils/fixed_axios.js
+++ client/src/utils/fixed_axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'process.env.FRONTEND-URL',
+  baseURL: process.env.FRONTEND-URL,
 });
 
 instance.interceptors.request.use((config) => {
